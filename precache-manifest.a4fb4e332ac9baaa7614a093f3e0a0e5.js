8719c444aa2263d873177206be61a8720e9f26cd
BartoszOrnaf <bartosz.ornaf@gmail.com>
2020-06-01 14:24:14 +0200

    Updates

---
+++ precache-manifest.a4fb4e332ac9baaa7614a093f3e0a0e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9934cc3a48486187c079dbcb552e161b",
+    "url": "/RickAndMorty/index.html"
+  },
+  {
+    "revision": "0052174798297b5e36b5",
+    "url": "/RickAndMorty/static/css/main.f8f62c9d.chunk.css"
+  },
+  {
+    "revision": "7f9952783070c77353ec",
+    "url": "/RickAndMorty/static/js/2.69f5241f.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/RickAndMorty/static/js/2.69f5241f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0052174798297b5e36b5",
+    "url": "/RickAndMorty/static/js/main.512715a7.chunk.js"
+  },
+  {
+    "revision": "ad02d3a87b89cbd447c9",
+    "url": "/RickAndMorty/static/js/runtime-main.fb311bad.js"
+  },
+  {
+    "revision": "54998b8fe09cde9eef76e5bf133a3120",
+    "url": "/RickAndMorty/static/media/draghere.54998b8f.png"
+  },
+  {
+    "revision": "fab025132fbbd7c6b419394b7fe56cad",
+    "url": "/RickAndMorty/static/media/loading.fab02513.png"
+  },
+  {
+    "revision": "081c19b2bd3e86f3a97bf57602e22e5c",
+    "url": "/RickAndMorty/static/media/loadingSmall.081c19b2.png"
+  },
+  {
+    "revision": "cb5e211cae67de1da6e43e484f82ec01",
+    "url": "/RickAndMorty/static/media/rickNodes.cb5e211c.png"
+  },
+  {
+    "revision": "3f89fed7f8cff923649162e1a1ff6044",
+    "url": "/RickAndMorty/static/media/rickSilent.3f89fed7.png"
+  },
+  {
+    "revision": "bd261ec4545ffefa1ec54a19683c0e48",
+    "url": "/RickAndMorty/static/media/rickWithBubble01.bd261ec4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RickAndMorty/precache-manifest.d5c8a5f90a588dbc4813ebed5ac43838.js"
+  "/RickAndMorty/precache-manifest.a4fb4e332ac9baaa7614a093f3e0a0e5.js"
 );
 
 self.addEventListener('message', (event) => {
